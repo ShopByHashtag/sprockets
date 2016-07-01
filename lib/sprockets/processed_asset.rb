6f9b2b93d52926cdab87127e54514bce8c0498f2
Robert Dilio <robert@relatednoise.com>
2016-07-01 13:12:17 -0400

    added airbrake and put in error handling to give me more info on asset error

--- lib/sprockets/processed_asset.rb
+++ lib/sprockets/processed_asset.rb
@@ -2,151 +2,172 @@ require 'sprockets/asset'
 require 'sprockets/utils'
 
 module Sprockets
-  class ProcessedAsset < Asset
-    def initialize(environment, logical_path, pathname)
-      super
+	class ProcessedAsset < Asset
+		def initialize(environment, logical_path, pathname)
+			super
 
-      start_time = Time.now.to_f
+			begin
+				start_time = Time.now.to_f
 
-      context = environment.context_class.new(environment, logical_path, pathname)
-      @source = context.evaluate(pathname)
-      @length = Rack::Utils.bytesize(source)
-      @digest = environment.digest.update(source).hexdigest
+				context = environment.context_class.new(environment, logical_path, pathname)
+				@source = context.evaluate(pathname)
+				@length = Rack::Utils.bytesize(source)
+				@digest = environment.digest.update(source).hexdigest
 
-      build_required_assets(environment, context)
-      build_dependency_paths(environment, context)
+				build_required_assets(environment, context) if context.present?
 
-      @dependency_digest = compute_dependency_digest(environment)
+				begin
+					build_dependency_paths(environment, context)
+				rescue => e
+					environment.logger.info "This path blew up and couldn't be found: #{logical_path}  (#{elapsed_time}ms)  (pid #{Process.pid})"
+					Airbrake.notify(e)
+				end
 
-      elapsed_time = ((Time.now.to_f - start_time) * 1000).to_i
-      environment.logger.info "Compiled #{logical_path}  (#{elapsed_time}ms)  (pid #{Process.pid})"
-    end
+				@dependency_digest = compute_dependency_digest(environment)
 
-    # Interal: Used to check equality
-    attr_reader :dependency_digest
+				elapsed_time = ((Time.now.to_f - start_time) * 1000).to_i
+				environment.logger.info "Compiled #{logical_path}  (#{elapsed_time}ms)  (pid #{Process.pid})"
+			rescue => e
+				environment.logger.info "There was a problem processing: #{logical_path}  (#{elapsed_time}ms)  (pid #{Process.pid})"
+				Airbrake.notify(e)
+			end
+		end
 
-    attr_reader :source
+		# Interal: Used to check equality
+		attr_reader :dependency_digest
 
-    # Initialize `BundledAsset` from serialized `Hash`.
-    def init_with(environment, coder)
-      super
+		attr_reader :source
 
-      @source = coder['source']
-      @dependency_digest = coder['dependency_digest']
+		# Initialize `BundledAsset` from serialized `Hash`.
+		def init_with(environment, coder)
+			super
 
-      @required_assets = coder['required_paths'].map { |p|
-        p = expand_root_path(p)
+			@source            = coder['source']
+			@dependency_digest = coder['dependency_digest']
 
-        unless environment.paths.detect { |path| p[path] }
-          raise UnserializeError, "#{p} isn't in paths"
-        end
+			@required_assets  = coder['required_paths'].map { |p|
+				p = expand_root_path(p)
 
-        p == pathname.to_s ? self : environment.find_asset(p, :bundle => false)
-      }
-      @dependency_paths = coder['dependency_paths'].map { |h|
-        DependencyFile.new(expand_root_path(h['path']), h['mtime'], h['digest'])
-      }
-    end
+				unless environment.paths.detect { |path| p[path] }
+					raise UnserializeError, "#{p} isn't in paths"
+				end
 
-    # Serialize custom attributes in `BundledAsset`.
-    def encode_with(coder)
-      super
+				p == pathname.to_s ? self : environment.find_asset(p, :bundle => false)
+			}
+			@dependency_paths = coder['dependency_paths'].map { |h|
+				DependencyFile.new(expand_root_path(h['path']), h['mtime'], h['digest'])
+			}
+		end
 
-      coder['source'] = source
-      coder['dependency_digest'] = dependency_digest
+		# Serialize custom attributes in `BundledAsset`.
+		def encode_with(coder)
+			super
 
-      coder['required_paths'] = required_assets.map { |a|
-        relativize_root_path(a.pathname).to_s
-      }
-      coder['dependency_paths'] = dependency_paths.map { |d|
-        { 'path' => relativize_root_path(d.pathname).to_s,
-          'mtime' => d.mtime.iso8601,
-          'digest' => d.digest }
-      }
-    end
+			coder['source']            = source
+			coder['dependency_digest'] = dependency_digest
 
-    # Checks if Asset is stale by comparing the actual mtime and
-    # digest to the inmemory model.
-    def fresh?(environment)
-      # Check freshness of all declared dependencies
-      @dependency_paths.all? { |dep| dependency_fresh?(environment, dep) }
-    end
+			coder['required_paths']   = required_assets.map { |a|
+				relativize_root_path(a.pathname).to_s
+			}
+			coder['dependency_paths'] = dependency_paths.map { |d|
+				{ 'path'   => relativize_root_path(d.pathname).to_s,
+				  'mtime'  => d.mtime.iso8601,
+				  'digest' => d.digest }
+			}
+		end
 
-    protected
-      class DependencyFile < Struct.new(:pathname, :mtime, :digest)
-        def initialize(pathname, mtime, digest)
-          pathname = Pathname.new(pathname) unless pathname.is_a?(Pathname)
-          mtime    = Time.parse(mtime) if mtime.is_a?(String)
-          super
-        end
+		# Checks if Asset is stale by comparing the actual mtime and
+		# digest to the inmemory model.
+		def fresh?(environment)
+			# Check freshness of all declared dependencies
+			@dependency_paths.all? { |dep| dependency_fresh?(environment, dep) }
+		end
 
-        def eql?(other)
-          other.is_a?(DependencyFile) &&
-            pathname.eql?(other.pathname) &&
-            mtime.eql?(other.mtime) &&
-            digest.eql?(other.digest)
-        end
+		protected
+		class DependencyFile < Struct.new(:pathname, :mtime, :digest)
+			def initialize(pathname, mtime, digest)
+				pathname = Pathname.new(pathname) unless pathname.is_a?(Pathname)
+				mtime    = Time.parse(mtime) if mtime.is_a?(String)
+				super
+			end
 
-        def hash
-          pathname.to_s.hash
-        end
-      end
+			def eql?(other)
+				other.is_a?(DependencyFile) &&
+					pathname.eql?(other.pathname) &&
+					mtime.eql?(other.mtime) &&
+					digest.eql?(other.digest)
+			end
 
-    private
-      def build_required_assets(environment, context)
-        @required_assets = resolve_dependencies(environment, context._required_paths + [pathname.to_s]) -
-          resolve_dependencies(environment, context._stubbed_assets.to_a)
-      end
+			def hash
+				pathname.to_s.hash
+			end
+		end
 
-      def resolve_dependencies(environment, paths)
-        assets = []
-        cache  = {}
+		private
+		def build_required_assets(environment, context)
+			@required_assets = resolve_dependencies(environment, context._required_paths + [pathname.to_s]) -
+				resolve_dependencies(environment, context._stubbed_assets.to_a)
+		end
 
-        paths.each do |path|
-          if path == self.pathname.to_s
-            unless cache[self]
-              cache[self] = true
-              assets << self
-            end
-          elsif asset = environment.find_asset(path, :bundle => false)
-            asset.required_assets.each do |asset_dependency|
-              unless cache[asset_dependency]
-                cache[asset_dependency] = true
-                assets << asset_dependency
-              end
-            end
-          end
-        end
+		def resolve_dependencies(environment, paths)
+			assets = []
+			cache  = {}
 
-        assets
-      end
+			paths.each do |path|
+				if path == self.pathname.to_s
+					unless cache[self]
+						cache[self] = true
+						assets << self
+					end
+				elsif asset = environment.find_asset(path, :bundle => false)
+					asset.required_assets.each do |asset_dependency|
+						unless cache[asset_dependency]
+							cache[asset_dependency] = true
+							assets << asset_dependency
+						end
+					end
+				end
+			end
 
-      def build_dependency_paths(environment, context)
-        dependency_paths = {}
+			assets
+		end
 
-        context._dependency_paths.each do |path|
-          dep = DependencyFile.new(path, environment.stat(path).mtime, environment.file_digest(path).hexdigest)
-          dependency_paths[dep] = true
-        end
+		def build_dependency_paths(environment, context)
+			dependency_paths = {}
 
-        context._dependency_assets.each do |path|
-          if path == self.pathname.to_s
-            dep = DependencyFile.new(pathname, environment.stat(path).mtime, environment.file_digest(path).hexdigest)
-            dependency_paths[dep] = true
-          elsif asset = environment.find_asset(path, :bundle => false)
-            asset.dependency_paths.each do |d|
-              dependency_paths[d] = true
-            end
-          end
-        end
+			context._dependency_paths.each { |path|
+				begin
+					dep                   = DependencyFile.new(path, environment.stat(path).mtime, environment.file_digest(path).hexdigest)
+					dependency_paths[dep] = true
+				rescue => e
+					environment.logger.info "There was a problem processing: #{path}!"
+					Airbrake.notify(e)
+				end
+			}
 
-        @dependency_paths = dependency_paths.keys
-      end
+			context._dependency_assets.each { |path|
+				begin
+					if path == self.pathname.to_s
+						dep                   = DependencyFile.new(pathname, environment.stat(path).mtime, environment.file_digest(path).hexdigest)
+						dependency_paths[dep] = true
+					elsif asset == environment.find_asset(path, :bundle => false)
+						asset.dependency_paths.each do |d|
+							dependency_paths[d] = true
+						end
+					end
+				rescue => e
+					environment.logger.info "There was a problem processing: #{path}!"
+					Airbrake.notify(e)
+				end
+			}
 
-      def compute_dependency_digest(environment)
-        required_assets.inject(environment.digest) { |digest, asset|
-          digest.update asset.digest
-        }.hexdigest
-      end
-  end
+			@dependency_paths = dependency_paths.keys
+		end
+
+		def compute_dependency_digest(environment)
+			required_assets.inject(environment.digest) { |digest, asset|
+				digest.update asset.digest
+			}.hexdigest
+		end
+	end
 end
--- sprockets.gemspec
+++ sprockets.gemspec
@@ -21,6 +21,7 @@ Gem::Specification.new do |s|
   s.add_development_dependency "ejs", "~> 1.0"
   s.add_development_dependency "execjs", "~> 1.0"
   s.add_development_dependency "json"
+  s.add_development_dependency "airbrake", "~> 5.0"
   s.add_development_dependency "rack-test"
   s.add_development_dependency "rake"
 
